3a9930ef8468557ea00165193690c89119989bd1
rifqyarifani <rifqy.arifani44@gmail.com>
2024-02-25 11:58:23 +0800

    change permohonan detail

--- app/permohonan/[id]/page.tsx
+++ app/permohonan/[id]/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import prisma from "@/prisma/client";
-import { NextResponse } from "next/server";
 import data from "../../../data.json";
 import * as querystring from "querystring";
-import { parse } from "path";
 
 interface Props {
   params: { id: string };
@@ -27,9 +24,11 @@ const DetailPage = async ({ params }: Props) => {
     <>
       <div>
         <h1>{permohonan.permohonan}</h1>
-        {permohonan.persyaratan.map((detail: any) => (
-          <h1>{detail}</h1>
-        ))}
+        <ul>
+          {permohonan.persyaratan.map((detail: any) => (
+            <li key={detail}>{detail}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
